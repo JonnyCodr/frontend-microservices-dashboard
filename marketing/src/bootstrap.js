19c616b1f65cc69fcf57c8bbf937148452ad1614
michael.menard@dhigroupinc.com <michael.menard@dhigroupinc.com>
2020-12-31 21:03:12 -0700

    adds navigation

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
+import { createMemoryHistory } from 'history'
 
-const mount = ( el ) => {
-    ReactDom.render( <App />, el );
+const mount = ( el, { onNavigate } ) => {
+    const history = createMemoryHistory();
+
+    if (onNavigate) {
+        history.listen(onNavigate)
+    }
+    ReactDom.render( <App history={history}/>, el );
+
+    return {
+        onParentNavigate({ pathname: nextPathname }) {
+            const { pathName } = history.location;
+
+            if (pathName !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    };
 };
 
 if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#marketing-dev-root');
 
     if (devRoot) {
-        mount(devRoot);
+        mount(devRoot, {});
     }
 }
 
